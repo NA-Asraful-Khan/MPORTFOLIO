cae3e635332cc51cd1ffdd2759f92276e96b571d
NA-Asraful-Khan <soyonshikhor@gmail.com>
2022-07-31 11:09:42 +0600

    Nav and main done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Main from "./Componant/Main";
 import Navbar from "./Componant/Navbar";
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Main></Main>
     </div>
   );
 }
